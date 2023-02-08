ccd960c2426103f072165f75b21873a223b8ca05
Will Murphy <68807296+Illogicalll@users.noreply.github.com>
2023-02-08 16:29:14 +0000

    Progress on dataValidation Program + OCR Data Exporting

--- dataValidation.py
+++ dataValidation.py
@@ -1,25 +1,9 @@
 from tkinter import *
 from PIL import ImageTk, Image
 import os
+import csv
+import pandas as pd
 
-# Button Functionality
-curimage = 0
-def nextim():
-    global curimage
-    curimage += 1
-    img1 = ImageTk.PhotoImage(Image.open(images[curimage]))
-    label.configure(image=img1)
-    label.photo = img1
-
-def updatestat():
-    stats = []
-    for obj in dobjects.keys():
-        if "entrybox" in obj:
-            if dobjects[obj].get() == "":
-                pass
-            else:
-                stats.append(dobjects[obj].get())
-    print(stats)
 
 # Creating List of All Card File Paths
 directories = [x[1] for x in os.walk(os.getcwd())]
@@ -28,7 +12,83 @@ sets = non_empty_dirs[8]
 images = []
 for set in sets:
     for card in os.listdir(f"Sets/{set}"):
-        images.append(f"Sets/{set}/{card}")
+        if card == "stats.csv":
+            pass
+        else:
+            images.append(f"Sets/{set}/{card}")
+
+# Button Functionality
+curimage = 0
+relativecurimage = 0
+def nextim():
+    global curimage
+    global relativecurimage
+    relativecurimage += 1
+    SET_DIR = os.path.abspath(images[curimage]).rstrip(images[curimage])
+    if relativecurimage == len(os.listdir(SET_DIR))-1:
+        for obj in dobjects.keys():
+            if "entrybox" in obj:
+                dobjects[obj].delete(0,END)
+        relativecurimage = 0
+    else:
+        for obj in dobjects.keys():
+            if "valueentrybox" in obj:
+                dobjects[obj].delete(0,END)
+            elif "nameentrybox0" in obj:
+                dobjects[obj].delete(0,END)
+    curimage += 1
+    img1 = ImageTk.PhotoImage(Image.open(images[curimage]))
+    label.configure(image=img1)
+    label.photo = img1
+    # LOAD DATA FROM OCR GENERATED STATS FILE
+    # WORKS BUT OCR KINDA SUCKS
+    curset = os.listdir(SET_DIR)
+    currow = []
+    with open(f"{SET_DIR}/stats.csv", 'r') as f:
+        rownum = curset.index(images[curimage].split('/')[2])
+        reader = csv.reader(f)
+        rows = list(reader)
+        currow = rows[rownum]
+    row = 0
+    for obj in dobjects.keys():
+        if "valueentrybox" in obj:
+            if row > len(currow):
+                break
+            try:
+                dobjects[obj].insert(0,currow[row])
+            except IndexError:
+                pass
+            row += 1
+
+def updatestat():
+    global relativecurimage
+    SET_DIR = os.path.abspath(images[curimage]).rstrip(images[curimage])
+    stats = ['Name']
+    for obj in dobjects.keys():
+        if "entrybox" in obj:
+            if dobjects[obj].get() == "":
+                pass
+            else:
+                stats.append(dobjects[obj].get())
+    with open(f"{SET_DIR}stats.csv", 'a') as f:
+        writer = csv.writer(f,delimiter=',', lineterminator='\n')
+        try:
+            pd.read_csv(f"{SET_DIR}stats.csv")
+        except:
+            writer.writerow(stats[::2])
+        writer.writerow(stats[1::2])
+    # EDITING OCR GENERATED STATS
+    # KINDA BROKEN
+    # data = []
+    # with open(f'{SET_DIR}ocrstats.csv', 'r') as f:
+    #     reader = csv.reader(f)
+    #     data.extend(reader)
+    # overrideline = {relativecurimage:stats}
+    # with open(f'{SET_DIR}stats.csv', 'w') as f:
+    #     writer = csv.writer(f)
+    #     for line, row in enumerate(data):
+    #         data = overrideline.get(line, row)
+    #         writer.writerow(data)
 
 # GUI
 win = Tk()
@@ -48,7 +108,9 @@ dobjects = {}
 formframe = Frame(win,width=300, height=400)
 formframe.pack()
 formframe.place(anchor='e', rely=0.5, relx=1)
-for i in range(6):
+dobjects["namelabel0"] = Label(formframe, text = f'Card Name')
+dobjects["nameentrybox0"] = Entry(formframe)
+for i in range(1,7):
     dobjects["namelabel{0}".format(i)] = Label(formframe, text = f'Stat {i} Name')
     dobjects["nameentrybox{0}".format(i)] = Entry(formframe)
     dobjects["valuelabel{0}".format(i)] = Label(formframe, text = f'Stat {i} Value')
--- statsOCR.py
+++ statsOCR.py
@@ -1,21 +1,46 @@
 import easyocr
 import cv2
 import numpy as np
+from autocorrect import Speller
+from country_list import countries_for_language
+import csv
+import os
 
 # Loading Image
-image = cv2.imread("Sets/1982 Top Trumps Cars/98647-6871984Fr.jpg")
+directories = [x[1] for x in os.walk(os.getcwd())]
+non_empty_dirs = [x for x in directories if x]
+sets = non_empty_dirs[8]
+for set in sets:
+    f = open(f"Sets/{set}/ocrstats.csv", 'w')
+    writer = csv.writer(f,delimiter=',', lineterminator='\n')
+    for card in os.listdir(f"Sets/{set}"):
+        if card == "ocrstats.csv":
+            break
+        image = cv2.imread(f"Sets/{set}/{card}")
 
-# OCR Readability Adjustments
-image2 = cv2.resize(image, (0,0), fx=1.5, fy=1.5)
-kernel = np.array([[0, -1, 0],
-                   [-1, 5,-1],
-                   [0, -1, 0]])
-image2 = cv2.filter2D(src=image2, ddepth=-1, kernel=kernel)
-# cv2.imshow('image',image2)
-# cv2.waitKey()
+        # OCR Readability Adjustments
+        image2 = cv2.resize(image, (0,0), fx=1.5, fy=1.5)
+        kernel = np.array([[0, -1, 0],
+                        [-1, 5,-1],
+                        [0, -1, 0]])
+        image2 = cv2.filter2D(src=image2, ddepth=-1, kernel=kernel)
+        # cv2.imshow('image',image2)
+        # cv2.waitKey()
 
-# Attempting to read stats off card
-reader = easyocr.Reader(['en'], gpu=False)
-result = reader.readtext(image2, detail=0)
-for detection in result:
-    print(detection)
+        # Attempting to read stats off card
+        reader = easyocr.Reader(['en'], gpu=False)
+        header = reader.readtext(image2, detail=0)
+        spell = Speller(lang='en')
+        for i in range (len(header)):
+            header[i] = spell(header[i])
+        stats = []
+        countries = dict(countries_for_language('en'))
+        for item in header:
+            if item.split(' ')[0].isdigit():
+                header.remove(item)
+                stats.append(item)
+            if item in countries.values():
+                header.remove(item)
+        writer.writerow(stats)
+    print(f"Finished {set}")
+    f.close()

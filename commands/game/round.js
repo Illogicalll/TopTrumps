88461b32919342da347476a8fdb2167cf81d8694
Will Murphy <68807296+Illogicalll@users.noreply.github.com>
2023-07-07 13:06:40 +0100

    Implemented Round Logic

--- commands/game/round.js
+++ commands/game/round.js
@@ -5,6 +5,25 @@ module.exports = {
 		.setName('round')
 		.setDescription('Get your hand'),
 	async execute() {
-		// Handle round
+		const roundStats = [];
+		global.userIDQueue.forEach(id => {
+			const servingUser = global.userIDQueue.indexOf(id);
+			global.interactionQueue[servingUser].followUp({ content: `**${global.usernameQueue[global.playerTurn]}** has chosen **${global.chosenStat}**`, ephemeral: true });
+			roundStats.push(parseInt(global.stats[parseInt(global.playerHands[servingUser][0].split('.')[0]) - 1][global.chosenStat]));
+		});
+		const roundWinner = roundStats.indexOf(Math.max(...roundStats));
+		let cardToMove = null;
+		const hasGivenCard = Array(global.userIDQueue.length).fill(false);
+		global.userIDQueue.forEach(id => {
+			if (roundWinner === global.userIDQueue.indexOf(id)) {
+				global.interactionQueue[roundWinner].followUp({ content: `**You win the round!** You now have **${global.playerHands[roundWinner].length + hasGivenCard.filter(given => given === false).length - 1}** cards!`, ephemeral: true });
+			}
+			else {
+				global.interactionQueue[global.userIDQueue.indexOf(id)].followUp({ content: `**${global.usernameQueue[roundWinner]} wins the round!** You now have **${global.playerHands[global.userIDQueue.indexOf(id)].length - 1}** cards...`, ephemeral: true });
+				cardToMove = global.playerHands[global.userIDQueue.indexOf(id)].shift();
+				global.playerHands[roundWinner].push(cardToMove);
+				hasGivenCard[global.userIDQueue.indexOf(id)] = true;
+			}
+		});
 	},
 };
